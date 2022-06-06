185706539bf6bbf723a050fa4e700f40d8fa11a4
bu <bu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-06 06:32:56 +0000

    Contract

---
+++ VOL.WebApi/Controllers/Contract/Partial/Viat_app_power_contract_custController.cs
@@ -0,0 +1,33 @@
+/*
+ *接口编写处...
+*如果接口需要做Action的权限验证，请在Action上使用属性
+*如: [ApiActionPermission("Viat_app_power_contract_cust",Enums.ActionPermissionOptions.Search)]
+ */
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using VOL.Entity.DomainModels;
+using VIAT.Contract.IServices;
+
+namespace VIAT.Contract.Controllers
+{
+    public partial class Viat_app_power_contract_custController
+    {
+        private readonly IViat_app_power_contract_custService _service;//访问业务代码
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        [ActivatorUtilitiesConstructor]
+        public Viat_app_power_contract_custController(
+            IViat_app_power_contract_custService service,
+            IHttpContextAccessor httpContextAccessor
+        )
+        : base(service)
+        {
+            _service = service;
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}
