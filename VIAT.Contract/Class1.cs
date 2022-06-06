089a19dd0263a5abd8fdeba5d21689b629566e34
bu <bu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-06 06:31:33 +0000

    Contract

---
+++ VIAT.Contract/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VIAT.Contract
+{
+    public class Class1
+    {
+    }
+}
---
+++ VIAT.Contract/IServices/Contract/Partial/IView_app_power_contract_mainService.cs
@@ -0,0 +1,13 @@
+/*
+*所有关于View_app_power_contract_main类的业务代码接口应在此处编写
+*/
+using VOL.Core.BaseProvider;
+using VOL.Entity.DomainModels;
+using VOL.Core.Utilities;
+using System.Linq.Expressions;
+namespace VIAT.Contract.IServices
+{
+    public partial interface IView_app_power_contract_mainService
+    {
+    }
+ }
