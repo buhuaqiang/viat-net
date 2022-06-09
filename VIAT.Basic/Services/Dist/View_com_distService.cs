cfa9ef20903ecf1b963f13b483fefd351ed17f96
ming <ming@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-09 07:49:37 +0000

    [Dev]經銷商客戶對應View與主表viat_com_dist之後端

---
+++ VIAT.Basic/Services/Dist/View_com_distService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下View_com_distService与IView_com_distService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class View_com_distService : ServiceBase<View_com_dist, IView_com_distRepository>
+    , IView_com_distService, IDependency
+    {
+    public View_com_distService(IView_com_distRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IView_com_distService Instance
+    {
+      get { return AutofacContainerModule.GetService<IView_com_distService>(); } }
+    }
+ }
---
+++ VOL.Entity/DomainModels/Dist/Viat_com_dist.cs
@@ -0,0 +1,144 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果数据库字段发生变化，请在代码生器重新生成此Model
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOL.Entity.SystemModels;
+
+namespace VOL.Entity.DomainModels
+{
+    [Entity(TableCnName = "經銷商客戶對應主表",TableName = "Viat_com_dist")]
+    public partial class Viat_com_dist:BaseEntity
+    {
+        /// <summary>
+       ///經銷商代碼
+       /// </summary>
+       [Display(Name ="經銷商代碼")]
+       [MaxLength(10)]
+       [Column(TypeName="varchar(10)")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public string dist_id { get; set; }
+
+       /// <summary>
+       ///識別碼
+       /// </summary>
+       [Display(Name ="識別碼")]
+       [Column(TypeName="int")]
+       [Required(AllowEmptyStrings=false)]
+       public int dbid { get; set; }
+
+       /// <summary>
+       ///建立用戶
+       /// </summary>
+       [Display(Name ="建立用戶")]
+       [MaxLength(200)]
+       [Column(TypeName="varchar(200)")]
+       public string Creator { get; set; }
+
+       /// <summary>
+       ///建立用戶ID
+       /// </summary>
+       [Display(Name ="建立用戶ID")]
+       [Column(TypeName="int")]
+       public int? CreateID { get; set; }
+
+       /// <summary>
+       ///建立時間
+       /// </summary>
+       [Display(Name ="建立時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? CreateDate { get; set; }
+
+       /// <summary>
+       ///建立者的委託人
+       /// </summary>
+       [Display(Name ="建立者的委託人")]
+       [Column(TypeName="int")]
+       [Editable(true)]
+       public int? CreateClient { get; set; }
+
+       /// <summary>
+       ///最後修改用戶ID
+       /// </summary>
+       [Display(Name ="最後修改用戶ID")]
+       [Column(TypeName="int")]
+       public int? ModifyID { get; set; }
+
+       /// <summary>
+       ///最後修改用戶
+       /// </summary>
+       [Display(Name ="最後修改用戶")]
+       [MaxLength(200)]
+       [Column(TypeName="varchar(200)")]
+       public string Modifier { get; set; }
+
+       /// <summary>
+       ///最後修改時間
+       /// </summary>
+       [Display(Name ="最後修改時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? ModifyDate { get; set; }
+
+       /// <summary>
+       ///最後修改者的委託人
+       /// </summary>
+       [Display(Name ="最後修改者的委託人")]
+       [Column(TypeName="int")]
+       public int? ModifyClient { get; set; }
+
+       /// <summary>
+       ///所屬事業單位,01:PH;03:AH;05:CH;06:NU
+       /// </summary>
+       [Display(Name ="所屬事業單位,01:PH;03:AH;05:CH;06:NU")]
+       [MaxLength(15)]
+       [Column(TypeName="varchar(15)")]
+       [Editable(true)]
+       public string division { get; set; }
+
+       /// <summary>
+       ///Expfizer Customer ID
+       /// </summary>
+       [Display(Name ="Expfizer Customer ID")]
+       [MaxLength(10)]
+       [Column(TypeName="varchar(10)")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public string cust_id_expfizer { get; set; }
+
+       /// <summary>
+       ///是否有效,Y: 是 ; N:否
+       /// </summary>
+       [Display(Name ="是否有效,Y: 是 ; N:否")]
+       [MaxLength(1)]
+       [Column(TypeName="varchar(1)")]
+       [Editable(true)]
+       public string status { get; set; }
+
+       /// <summary>
+       ///公司別,舊版SUN_DB
+       /// </summary>
+       [Display(Name ="公司別,舊版SUN_DB")]
+       [MaxLength(3)]
+       [Column(TypeName="varchar(3)")]
+       [Editable(true)]
+       public string entity { get; set; }
+
+       /// <summary>
+       ///主鍵
+       /// </summary>
+       [Key]
+       [Display(Name ="主鍵")]
+       [Column(TypeName="uniqueidentifier")]
+       [Required(AllowEmptyStrings=false)]
+       public Guid dist_dbid { get; set; }
+
+       
+    }
+}
