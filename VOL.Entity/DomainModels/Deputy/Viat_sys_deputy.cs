3038d875499740599d7e70948ab34249064c4347
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-10 01:22:19 +0000

    新增代理人

---
+++ VOL.Entity/DomainModels/Deputy/Viat_sys_deputy.cs
@@ -0,0 +1,176 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果数据库字段发生变化，请在代码生器重新生成此Model
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOL.Entity.SystemModels;
+
+namespace VOL.Entity.DomainModels
+{
+    [Entity(TableCnName = "代理人清單",TableName = "Viat_sys_deputy")]
+    public partial class Viat_sys_deputy:BaseEntity
+    {
+        /// <summary>
+       ///PKID
+       /// </summary>
+       [Key]
+       [Display(Name ="PKID")]
+       [Column(TypeName="uniqueidentifier")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public Guid deputy_dbid { get; set; }
+
+       /// <summary>
+       ///識別碼, PK, Identity
+       /// </summary>
+      /* [Display(Name ="識別碼, PK, Identity")]
+       [Column(TypeName="int")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public int dbid { get; set; }*/
+
+       /// <summary>
+       ///公司別,舊版SUN_DB
+       /// </summary>
+       [Display(Name ="公司別,舊版SUN_DB")]
+       [MaxLength(3)]
+       [Column(TypeName="varchar(3)")]
+       public string entity { get; set; }
+
+       /// <summary>
+       ///所屬事業單位,01:PH;03:AH;05:CH;06:NU
+       /// </summary>
+       [Display(Name ="所屬事業單位,01:PH;03:AH;05:CH;06:NU")]
+       [MaxLength(15)]
+       [Column(TypeName="varchar(15)")]
+       public string division { get; set; }
+
+       /// <summary>
+       ///用戶表PKI
+       /// </summary>
+       [Display(Name ="用戶表PKI")]
+       [Column(TypeName="int")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public int user_id { get; set; }
+
+       /// <summary>
+       ///代理人用戶PKID
+       /// </summary>
+       [Display(Name ="代理人用戶PKID")]
+       [Column(TypeName="int")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public int deputy_user_id { get; set; }
+
+       /// <summary>
+       ///代理起始日
+       /// </summary>
+       [Display(Name ="代理起始日")]
+       [Column(TypeName="datetime")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public DateTime start_date { get; set; }
+
+       /// <summary>
+       ///代理結束日
+       /// </summary>
+       [Display(Name ="代理結束日")]
+       [Column(TypeName="datetime")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public DateTime end_date { get; set; }
+
+       /// <summary>
+       ///是否有效,Y:生效/N:未生效or期滿/C:移除
+       /// </summary>
+       [Display(Name ="是否有效,Y:生效/N:未生效or期滿/C:移除")]
+       [MaxLength(1)]
+       [Column(TypeName="varchar(1)")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public string status { get; set; }
+
+       /// <summary>
+       ///建立用戶
+       /// </summary>
+       [Display(Name ="建立用戶")]
+       [Column(TypeName="int")]
+       public int? created_user { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="created_username")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string created_username { get; set; }
+
+       /// <summary>
+       ///建立者的委託人
+       /// </summary>
+       [Display(Name ="建立者的委託人")]
+       [Column(TypeName="int")]
+       public int? created_client { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="created_clientusername")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string created_clientusername { get; set; }
+
+       /// <summary>
+       ///建立時間
+       /// </summary>
+       [Display(Name ="建立時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? created_date { get; set; }
+
+       /// <summary>
+       ///最後修改用戶
+       /// </summary>
+       [Display(Name ="最後修改用戶")]
+       [Column(TypeName="int")]
+       public int? modified_user { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="modified_username")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string modified_username { get; set; }
+
+       /// <summary>
+       ///最後修改者的委託人
+       /// </summary>
+       [Display(Name ="最後修改者的委託人")]
+       [Column(TypeName="int")]
+       public int? modified_client { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="modified_clientusername")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string modified_clientusername { get; set; }
+
+       /// <summary>
+       ///最後修改時間
+       /// </summary>
+       [Display(Name ="最後修改時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? modified_date { get; set; }
+
+       
+    }
+}
---
+++ VOL.Entity/DomainModels/Deputy/View_sys_deputy_pop.cs
@@ -0,0 +1,70 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果数据库字段发生变化，请在代码生器重新生成此Model
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOL.Entity.SystemModels;
+
+namespace VOL.Entity.DomainModels
+{
+    [Entity(TableCnName = "代理人pop框",TableName = "View_sys_deputy_pop")]
+    public partial class View_sys_deputy_pop:BaseEntity
+    {
+        /// <summary>
+       ///
+       /// </summary>
+       [Key]
+       [Display(Name ="user_id")]
+       [Column(TypeName="int")]
+       [Required(AllowEmptyStrings=false)]
+       public int user_id { get; set; }
+
+       /// <summary>
+       ///UserName
+       /// </summary>
+       [Display(Name ="UserName")]
+       [MaxLength(20)]
+       [Column(TypeName="varchar(20)")]
+       [Required(AllowEmptyStrings=false)]
+       public string user_name { get; set; }
+
+       /// <summary>
+       ///Department Name
+       /// </summary>
+       [Display(Name ="Department Name")]
+       [MaxLength(255)]
+       [Column(TypeName="nvarchar(255)")]
+       public string dept_name { get; set; }
+
+       /// <summary>
+       ///E-Name
+       /// </summary>
+       [Display(Name ="E-Name")]
+       [MaxLength(40)]
+       [Column(TypeName="varchar(40)")]
+       public string emp_ename { get; set; }
+
+       /// <summary>
+       ///C-Name
+       /// </summary>
+       [Display(Name ="C-Name")]
+       [MaxLength(50)]
+       [Column(TypeName="nvarchar(50)")]
+       public string emp_cname { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="CreateDate")]
+       [Column(TypeName="datetime")]
+       public DateTime? CreateDate { get; set; }
+
+       
+    }
+}
