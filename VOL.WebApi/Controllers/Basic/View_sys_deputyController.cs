8cd0bb4f364f886f6fb4c3d8b9f8572361793a32
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-10 01:23:41 +0000

    新增代理人

---
+++ VOL.WebApi/Controllers/Basic/View_sys_deputyController.cs
@@ -0,0 +1,23 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果要增加方法请在当前目录下Partial文件夹View_sys_deputyController编写
+ */
+using Microsoft.AspNetCore.Mvc;
+using VOL.Core.Controllers.Basic;
+using VOL.Entity.AttributeManager;
+using VIAT.Basic.IServices;
+using VOL.Core.Filters;
+using VOL.Entity.DomainModels;
+namespace VIAT.Basic.Controllers
+{
+    [Route("api/View_sys_deputy")]
+    [PermissionTable(Name = "View_sys_deputy")]
+    public partial class View_sys_deputyController : ApiBaseController<IView_sys_deputyService>
+    {
+        public View_sys_deputyController(IView_sys_deputyService service)
+        : base(service)
+        {
+        }
+    }
+}
+
