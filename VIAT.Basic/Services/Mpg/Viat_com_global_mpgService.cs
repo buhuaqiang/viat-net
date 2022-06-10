76e7d040667ecc8bac58af33fab3c06ea662f6ae
ypj <ypj@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-10 01:09:27 +0000

    MPG

---
+++ VIAT.Basic/Services/Mpg/Viat_com_global_mpgService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下Viat_com_global_mpgService与IViat_com_global_mpgService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class Viat_com_global_mpgService : ServiceBase<Viat_com_global_mpg, IViat_com_global_mpgRepository>
+    , IViat_com_global_mpgService, IDependency
+    {
+    public Viat_com_global_mpgService(IViat_com_global_mpgRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IViat_com_global_mpgService Instance
+    {
+      get { return AutofacContainerModule.GetService<IViat_com_global_mpgService>(); } }
+    }
+ }
---
+++ VIAT.Basic/Services/Mpg/Viat_com_local_mpgService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下Viat_com_local_mpgService与IViat_com_local_mpgService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class Viat_com_local_mpgService : ServiceBase<Viat_com_local_mpg, IViat_com_local_mpgRepository>
+    , IViat_com_local_mpgService, IDependency
+    {
+    public Viat_com_local_mpgService(IViat_com_local_mpgRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IViat_com_local_mpgService Instance
+    {
+      get { return AutofacContainerModule.GetService<IViat_com_local_mpgService>(); } }
+    }
+ }
