f4087c5ca5652a23f76ba9cc22245994e3fa6814
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-10 01:21:41 +0000

    新增代理人

---
+++ VIAT.Basic/Services/Deputy/Partial/View_sys_deputyService.cs
@@ -0,0 +1,61 @@
+/*
+ *所有关于View_sys_deputy类的业务代码应在此处编写
+*可使用repository.调用常用方法，获取EF/Dapper等信息
+*如果需要事务请使用repository.DbContextBeginTransaction
+*也可使用DBServerProvider.手动获取数据库相关信息
+*用户信息、权限、角色等使用UserContext.Current操作
+*View_sys_deputyService对增、删、改查、导入、导出、审核业务代码扩展参照ServiceFunFilter
+*/
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+using System.Linq;
+using VOL.Core.Utilities;
+using System.Linq.Expressions;
+using VOL.Core.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+
+namespace VIAT.Basic.Services
+{
+    public partial class View_sys_deputyService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IView_sys_deputyRepository _repository;//访问数据库
+        private readonly IViat_sys_deputyService _viat_sys_DeputyService;
+        private readonly IViat_sys_deputyRepository _viat_Sys_DeputyRepository;
+        WebResponseContent webResponse = new WebResponseContent();
+
+        [ActivatorUtilitiesConstructor]
+        public View_sys_deputyService(
+            IView_sys_deputyRepository dbRepository,
+            IHttpContextAccessor httpContextAccessor,
+            IViat_sys_deputyService viat_Sys_Deputy,
+            IViat_sys_deputyRepository viat_Sys_DeputyRepository
+            )
+        : base(dbRepository)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _repository = dbRepository;
+            //多租户会用到这init代码，其他情况可以不用
+            //base.Init(dbRepository);
+            _viat_sys_DeputyService = viat_Sys_Deputy;
+            _viat_Sys_DeputyRepository = viat_Sys_DeputyRepository;
+        }
+
+        public override WebResponseContent Add(SaveModel saveDataModel)
+        {
+            saveDataModel.MainData["status"] = "Y";
+            return _viat_sys_DeputyService.Add(saveDataModel);
+        }
+
+        //重写更新方法，更新Viat_com_prod表
+        public override WebResponseContent Update(SaveModel saveModel)
+        {
+            return _viat_sys_DeputyService.Update(saveModel);
+        }
+    }
+}
---
+++ VIAT.Basic/Services/Deputy/View_sys_deputy_popService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下View_sys_deputy_popService与IView_sys_deputy_popService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class View_sys_deputy_popService : ServiceBase<View_sys_deputy_pop, IView_sys_deputy_popRepository>
+    , IView_sys_deputy_popService, IDependency
+    {
+    public View_sys_deputy_popService(IView_sys_deputy_popRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IView_sys_deputy_popService Instance
+    {
+      get { return AutofacContainerModule.GetService<IView_sys_deputy_popService>(); } }
+    }
+ }
