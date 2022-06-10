e400dd4467455fbccba64c5cb419517791b6e91a
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-10 01:21:41 +0000

    新增代理人

---
+++ Vue.Net/VIAT.Basic/Services/Deputy/Viat_sys_deputyService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下Viat_sys_deputyService与IViat_sys_deputyService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class Viat_sys_deputyService : ServiceBase<Viat_sys_deputy, IViat_sys_deputyRepository>
+    , IViat_sys_deputyService, IDependency
+    {
+    public Viat_sys_deputyService(IViat_sys_deputyRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IViat_sys_deputyService Instance
+    {
+      get { return AutofacContainerModule.GetService<IViat_sys_deputyService>(); } }
+    }
+ }
---
+++ Vue.Net/VIAT.Basic/Services/Deputy/View_sys_deputyService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下View_sys_deputyService与IView_sys_deputyService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class View_sys_deputyService : ServiceBase<View_sys_deputy, IView_sys_deputyRepository>
+    , IView_sys_deputyService, IDependency
+    {
+    public View_sys_deputyService(IView_sys_deputyRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IView_sys_deputyService Instance
+    {
+      get { return AutofacContainerModule.GetService<IView_sys_deputyService>(); } }
+    }
+ }
