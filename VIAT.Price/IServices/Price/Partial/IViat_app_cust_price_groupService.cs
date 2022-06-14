4be943abb427b07b493388047046163d79905699
ypj <ypj@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-14 02:56:27 +0000

    價格群組

---
+++ VIAT.Price/IServices/Price/Partial/IViat_app_cust_price_groupService.cs
@@ -0,0 +1,13 @@
+/*
+*所有关于Viat_app_cust_price_group类的业务代码接口应在此处编写
+*/
+using VOL.Core.BaseProvider;
+using VOL.Entity.DomainModels;
+using VOL.Core.Utilities;
+using System.Linq.Expressions;
+namespace VIAT.Price.IServices
+{
+    public partial interface IViat_app_cust_price_groupService
+    {
+    }
+ }
---
+++ VIAT.Price/Services/Price/Viat_app_cust_price_groupService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下Viat_app_cust_price_groupService与IViat_app_cust_price_groupService中编写
+ */
+using VIAT.Price.IRepositories;
+using VIAT.Price.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Price.Services
+{
+    public partial class Viat_app_cust_price_groupService : ServiceBase<Viat_app_cust_price_group, IViat_app_cust_price_groupRepository>
+    , IViat_app_cust_price_groupService, IDependency
+    {
+    public Viat_app_cust_price_groupService(IViat_app_cust_price_groupRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IViat_app_cust_price_groupService Instance
+    {
+      get { return AutofacContainerModule.GetService<IViat_app_cust_price_groupService>(); } }
+    }
+ }
