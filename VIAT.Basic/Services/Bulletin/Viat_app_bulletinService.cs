1e9ca990c51d90e845b01089cbd32a2610f817c0
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-15 02:25:44 +0000

    系統訊息設置

---
+++ VIAT.Basic/Services/Bulletin/Viat_app_bulletinService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下Viat_app_bulletinService与IViat_app_bulletinService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class Viat_app_bulletinService : ServiceBase<Viat_app_bulletin, IViat_app_bulletinRepository>
+    , IViat_app_bulletinService, IDependency
+    {
+    public Viat_app_bulletinService(IViat_app_bulletinRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IViat_app_bulletinService Instance
+    {
+      get { return AutofacContainerModule.GetService<IViat_app_bulletinService>(); } }
+    }
+ }
---
+++ VIAT.Basic/Services/Bulletin/View_com_bulletinService.cs
@@ -0,0 +1,27 @@
+/*
+ *Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,此处任何更改都可能导致被代码生成器覆盖
+ *所有业务编写全部应在Partial文件夹下View_com_bulletinService与IView_com_bulletinService中编写
+ */
+using VIAT.Basic.IRepositories;
+using VIAT.Basic.IServices;
+using VOL.Core.BaseProvider;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Services
+{
+    public partial class View_com_bulletinService : ServiceBase<View_com_bulletin, IView_com_bulletinRepository>
+    , IView_com_bulletinService, IDependency
+    {
+    public View_com_bulletinService(IView_com_bulletinRepository repository)
+    : base(repository)
+    {
+    Init(repository);
+    }
+    public static IView_com_bulletinService Instance
+    {
+      get { return AutofacContainerModule.GetService<IView_com_bulletinService>(); } }
+    }
+ }
