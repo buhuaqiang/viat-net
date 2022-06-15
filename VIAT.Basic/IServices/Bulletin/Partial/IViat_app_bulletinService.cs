df9eeeef36c13769401ad1b201ccbb7d765646dc
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-15 02:24:21 +0000

    系統訊息設置

---
+++ VIAT.Basic/IServices/Bulletin/Partial/IViat_app_bulletinService.cs
@@ -0,0 +1,13 @@
+/*
+*所有关于Viat_app_bulletin类的业务代码接口应在此处编写
+*/
+using VOL.Core.BaseProvider;
+using VOL.Entity.DomainModels;
+using VOL.Core.Utilities;
+using System.Linq.Expressions;
+namespace VIAT.Basic.IServices
+{
+    public partial interface IViat_app_bulletinService
+    {
+    }
+ }
