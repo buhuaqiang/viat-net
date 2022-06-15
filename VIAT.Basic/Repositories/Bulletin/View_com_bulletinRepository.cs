2b84f7c89468c4a5f73be0d49434720089b15791
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-15 02:25:03 +0000

    系統訊息設置

---
+++ VIAT.Basic/Repositories/Bulletin/View_com_bulletinRepository.cs
@@ -0,0 +1,24 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *Repository提供数据库操作，如果要增加数据库操作请在当前目录下Partial文件夹View_com_bulletinRepository编写代码
+ */
+using VIAT.Basic.IRepositories;
+using VOL.Core.BaseProvider;
+using VOL.Core.EFDbContext;
+using VOL.Core.Extensions.AutofacManager;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Basic.Repositories
+{
+    public partial class View_com_bulletinRepository : RepositoryBase<View_com_bulletin> , IView_com_bulletinRepository
+    {
+    public View_com_bulletinRepository(VOLContext dbContext)
+    : base(dbContext)
+    {
+
+    }
+    public static IView_com_bulletinRepository Instance
+    {
+      get {  return AutofacContainerModule.GetService<IView_com_bulletinRepository>(); } }
+    }
+}
