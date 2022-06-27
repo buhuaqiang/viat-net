490d366b3f5ce976df7f75bb9da070887afc8bae
wsyu <wsyu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-27 01:36:28 +0000

    HP功能提交

---
+++ Vue.Net/VOL.WebApi/Controllers/Contract/Viat_app_hp_contract_free_prodController.cs
@@ -0,0 +1,32 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果要增加方法请在当前目录下Partial文件夹Viat_app_hp_contract_custController编写
+ */
+using Microsoft.AspNetCore.Mvc;
+using VOL.Core.Controllers.Basic;
+using VOL.Entity.AttributeManager;
+using VIAT.Contract.IServices;
+using VOL.Core.Filters;
+using VOL.Entity.DomainModels;
+
+namespace VIAT.Contract.Controllers
+{
+    [Route("api/Viat_app_hp_contract_free_prod")]
+    [PermissionTable(Name = "Viat_app_hp_contract_free_prod")]
+    public partial class Viat_app_hp_contract_free_prodController : ApiBaseController<IViat_app_hp_contract_free_prodService>
+    {
+        public Viat_app_hp_contract_free_prodController(IViat_app_hp_contract_free_prodService service)
+        : base(service)
+        {
+        }
+
+        [HttpPost, Route("GetPageData")]
+        [ApiActionPermission()]
+        public override ActionResult GetPageData([FromBody] PageDataOptions loadData)
+        {
+
+            return base.GetPageData(loadData);
+        }
+    }
+}
+
