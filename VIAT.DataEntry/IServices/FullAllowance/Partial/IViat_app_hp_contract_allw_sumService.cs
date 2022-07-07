6edd1b2e54a1204b4f06fbee4f828770a9798457
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-07 01:51:50 +0000

    reverse新增

---
+++ VIAT.DataEntry/IServices/FullAllowance/Partial/IViat_app_hp_contract_allw_sumService.cs
@@ -0,0 +1,13 @@
+/*
+*所有关于Viat_app_hp_contract_allw_sum类的业务代码接口应在此处编写
+*/
+using VIAT.Core.BaseProvider;
+using VIAT.Entity.DomainModels;
+using VIAT.Core.Utilities;
+using System.Linq.Expressions;
+namespace VIAT.DataEntry.IServices
+{
+    public partial interface IViat_app_hp_contract_allw_sumService
+    {
+    }
+ }
