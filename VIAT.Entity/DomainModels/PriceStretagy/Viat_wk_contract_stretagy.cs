51b8c43ba45b94dda045dad9208392cfe6f9ed22
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-13 06:11:16 +0000

    價格策略合約

---
+++ VIAT.Entity/DomainModels/PriceStretagy/Viat_wk_contract_stretagy.cs
@@ -0,0 +1,143 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果数据库字段发生变化，请在代码生器重新生成此Model
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VIAT.Entity.SystemModels;
+
+namespace VIAT.Entity.DomainModels
+{
+    [Entity(TableCnName = "價格策略合約",TableName = "Viat_wk_contract_stretagy")]
+    public partial class Viat_wk_contract_stretagy:BaseEntity
+    {
+        /// <summary>
+       ///PKID
+       /// </summary>
+       [Key]
+       [Display(Name ="PKID")]
+       [Column(TypeName="uniqueidentifier")]
+       [Required(AllowEmptyStrings=false)]
+       public Guid contstret_dbid { get; set; }
+
+       /// <summary>
+       ///Stretagy ID
+       /// </summary>
+       [Display(Name ="Stretagy ID")]
+       [MaxLength(10)]
+       [Column(TypeName="varchar(10)")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public string cont_stretagy_id { get; set; }
+
+       /// <summary>
+       ///Stretagy Name
+       /// </summary>
+       [Display(Name ="Stretagy Name")]
+       [MaxLength(100)]
+       [Column(TypeName="varchar(100)")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public string cont_stretagy_name { get; set; }
+
+       /// <summary>
+       ///Amount
+       /// </summary>
+       [Display(Name ="Amount")]
+       [Column(TypeName="numeric")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public decimal amount { get; set; }
+
+       /// <summary>
+       ///Status
+       /// </summary>
+       [Display(Name ="Status")]
+       [MaxLength(1)]
+       [Column(TypeName="char(1)")]
+       [Editable(true)]
+       [Required(AllowEmptyStrings=false)]
+       public string status { get; set; }
+
+       /// <summary>
+       ///建立用戶
+       /// </summary>
+       [Display(Name ="建立用戶")]
+       [Column(TypeName="int")]
+       public int? created_user { get; set; }
+
+       /// <summary>
+       ///列created_username
+       /// </summary>
+       [Display(Name ="列created_username")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string created_username { get; set; }
+
+       /// <summary>
+       ///建立者的委託人
+       /// </summary>
+       [Display(Name ="建立者的委託人")]
+       [Column(TypeName="int")]
+       public int? created_client { get; set; }
+
+       /// <summary>
+       ///列created_clientusername
+       /// </summary>
+       [Display(Name ="列created_clientusername")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string created_clientusername { get; set; }
+
+       /// <summary>
+       ///建立時間
+       /// </summary>
+       [Display(Name ="建立時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? created_date { get; set; }
+
+       /// <summary>
+       ///最後修改用戶
+       /// </summary>
+       [Display(Name ="最後修改用戶")]
+       [Column(TypeName="int")]
+       public int? modified_user { get; set; }
+
+       /// <summary>
+       ///列modified_username
+       /// </summary>
+       [Display(Name ="列modified_username")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string modified_username { get; set; }
+
+       /// <summary>
+       ///最後修改者的委託人
+       /// </summary>
+       [Display(Name ="最後修改者的委託人")]
+       [Column(TypeName="int")]
+       public int? modified_client { get; set; }
+
+       /// <summary>
+       ///列modified_clientusername
+       /// </summary>
+       [Display(Name ="列modified_clientusername")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string modified_clientusername { get; set; }
+
+       /// <summary>
+       ///最後修改時間
+       /// </summary>
+       [Display(Name ="最後修改時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? modified_date { get; set; }
+
+       
+    }
+}
