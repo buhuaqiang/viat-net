342b4085702c720e25f3a1bb95af5fcca049fee8
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-18 10:27:46 +0000

    客户申请资料

---
+++ Vue.Net/VIAT.WorkFlow/IServices/CustApply/IViat_wk_custService.cs
@@ -0,0 +1,12 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ */
+using VIAT.Core.BaseProvider;
+using VIAT.Entity.DomainModels;
+
+namespace VIAT.WorkFlow.IServices
+{
+    public partial interface IViat_wk_custService : IService<Viat_wk_cust>
+    {
+    }
+}
