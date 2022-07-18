e650c491057af88d105a74a7dfe8152a37c943ce
bu <bu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-18 10:44:22 +0000

    Bid Price

---
+++ Vue.Net/VIAT.WorkFlow/IServices/Bid/Partial/IViat_wk_bid_detailService.cs
@@ -0,0 +1,13 @@
+/*
+*所有关于Viat_wk_bid_detail类的业务代码接口应在此处编写
+*/
+using VIAT.Core.BaseProvider;
+using VIAT.Entity.DomainModels;
+using VIAT.Core.Utilities;
+using System.Linq.Expressions;
+namespace VIAT.WorkFlow.IServices
+{
+    public partial interface IViat_wk_bid_detailService
+    {
+    }
+ }
