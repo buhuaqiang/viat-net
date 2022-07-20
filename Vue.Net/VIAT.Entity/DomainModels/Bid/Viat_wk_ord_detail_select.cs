be8faf5c697814cb2a2a39ec840981854f989370
bu <bu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-20 12:14:24 +0000

    Bid Price

---
+++ Vue.Net/VIAT.Entity/DomainModels/Bid/Viat_wk_ord_detail_select.cs
@@ -0,0 +1,149 @@
+/*
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *如果数据库字段发生变化，请在代码生器重新生成此Model
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VIAT.Entity.SystemModels;
+
+namespace VIAT.Entity.DomainModels
+{
+    [Entity(TableCnName = "Viat_wk_ord_detail_訂單明細",TableName = "Viat_wk_ord_detail")]
+    public partial class Viat_wk_ord_detail_select:BaseEntity
+    {
+        /// <summary>
+       ///
+       /// </summary>
+       [Key]
+       [Display(Name ="ordetail_dbid")]
+       [Column(TypeName="uniqueidentifier")]
+       [Required(AllowEmptyStrings=false)]
+       public Guid ordetail_dbid { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="bidmast_dbid")]
+       [Column(TypeName="uniqueidentifier")]
+       public Guid? bidmast_dbid { get; set; }
+
+       /// <summary>
+       ///PKID
+       /// </summary>
+       [Display(Name ="PKID")]
+       [Column(TypeName="uniqueidentifier")]
+       public Guid? prod_dbid { get; set; }
+
+
+        /// <summary>
+        ///產品ID
+        /// </summary> 
+        [Column(TypeName = "prod_id")]
+        [Required(AllowEmptyStrings = false)]
+        public string prod_id { get; set; }
+
+        /// <summary>
+        ///產品名
+        /// </summary> 
+        [Column(TypeName = "prod_ename")]
+        [Required(AllowEmptyStrings = false)]
+        public string prod_ename { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Display(Name ="qty")]
+       [Column(TypeName="int")]
+       [Required(AllowEmptyStrings=false)]
+       public int qty { get; set; }
+
+       /// <summary>
+       ///
+       /// </summary>
+       [Display(Name ="import_flag")]
+       [MaxLength(10)]
+       [Column(TypeName="char(10)")]
+       public string import_flag { get; set; }
+
+       /// <summary>
+       ///建立用戶
+       /// </summary>
+       [Display(Name ="建立用戶")]
+       [Column(TypeName="int")]
+       public int? created_user { get; set; }
+
+       /// <summary>
+       ///建立用戶
+       /// </summary>
+       [Display(Name ="建立用戶")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string created_username { get; set; }
+
+       /// <summary>
+       ///建立者的委託人
+       /// </summary>
+       [Display(Name ="建立者的委託人")]
+       [Column(TypeName="int")]
+       public int? created_client { get; set; }
+
+       /// <summary>
+       ///建立者的委託人
+       /// </summary>
+       [Display(Name ="建立者的委託人")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string created_clientusername { get; set; }
+
+       /// <summary>
+       ///建立時間
+       /// </summary>
+       [Display(Name ="建立時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? created_date { get; set; }
+
+       /// <summary>
+       ///最後修改用戶
+       /// </summary>
+       [Display(Name ="最後修改用戶")]
+       [Column(TypeName="int")]
+       public int? modified_user { get; set; }
+
+       /// <summary>
+       ///最後修改用戶
+       /// </summary>
+       [Display(Name ="最後修改用戶")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string modified_username { get; set; }
+
+       /// <summary>
+       ///最後修改者的委託人
+       /// </summary>
+       [Display(Name ="最後修改者的委託人")]
+       [Column(TypeName="int")]
+       public int? modified_client { get; set; }
+
+       /// <summary>
+       ///最後修改者的委託人
+       /// </summary>
+       [Display(Name ="最後修改者的委託人")]
+       [MaxLength(50)]
+       [Column(TypeName="varchar(50)")]
+       public string modified_clientusername { get; set; }
+
+       /// <summary>
+       ///最後修改時間
+       /// </summary>
+       [Display(Name ="最後修改時間")]
+       [Column(TypeName="datetime")]
+       public DateTime? modified_date { get; set; }
+
+       
+    }
+}
