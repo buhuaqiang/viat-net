5a3f400da8f2a3ed7cc35c17915e680cbd45bc37
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-20 03:55:58 +0000

    重新方法

--- VIAT.DataEntry/Services/FullAllowance/Partial/View_full_allowance_adjustmentService.cs
+++ VIAT.DataEntry/Services/FullAllowance/Partial/View_full_allowance_adjustmentService.cs
@@ -17,6 +17,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using VIAT.DataEntry.IRepositories;
+using VIAT.DataEntry.IServices;
+using System.Collections.Generic;
 
 namespace VIAT.DataEntry.Services
 {
@@ -24,18 +26,47 @@ namespace VIAT.DataEntry.Services
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IView_full_allowance_adjustmentRepository _repository;//访问数据库
+        private readonly IViat_app_hp_contract_allw_sumService _viat_app_hp_contract_allw_sumService;
+        private readonly IViat_app_hp_contract_allw_sumRepository _viat_app_hp_contract_allw_sumRepository;
+        WebResponseContent webResponse = new WebResponseContent();
 
         [ActivatorUtilitiesConstructor]
         public View_full_allowance_adjustmentService(
             IView_full_allowance_adjustmentRepository dbRepository,
-            IHttpContextAccessor httpContextAccessor
+            IHttpContextAccessor httpContextAccessor,
+            IViat_app_hp_contract_allw_sumService viat_app_hp_contract_allw_sumService,
+            IViat_app_hp_contract_allw_sumRepository viat_app_hp_contract_allw_sumRepository
             )
         : base(dbRepository)
         {
             _httpContextAccessor = httpContextAccessor;
             _repository = dbRepository;
+            _viat_app_hp_contract_allw_sumService = viat_app_hp_contract_allw_sumService;
+            _viat_app_hp_contract_allw_sumRepository = viat_app_hp_contract_allw_sumRepository;
             //多租户会用到这init代码，其他情况可以不用
             //base.Init(dbRepository);
         }
-  }
+
+
+        public override WebResponseContent Add(SaveModel saveDataModel)
+        {
+            // 在保存数据库前的操作，所有数据都验证通过了，这一步执行完就执行数据库保存
+            saveDataModel.MainData["action_type"] = '3';
+            return _viat_app_hp_contract_allw_sumService.Add(saveDataModel);
+        }
+
+        public override WebResponseContent Update(SaveModel saveModel)
+        {
+            UpdateOnExecuting = (View_full_allowance_adjustment order, object addList, object updateList, List<object> delKeys) =>
+            {
+                return webResponse.OK();
+            };
+            return _viat_app_hp_contract_allw_sumService.Update(saveModel);
+        }
+
+        public override WebResponseContent Del(object[] keys, bool delList = true)
+        {
+            return _viat_app_hp_contract_allw_sumService.Del(keys, delList);
+        }
+    }
 }
