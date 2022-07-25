55ef6d3f178361f3857b55216df4b304d3774353
wsyu <wsyu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-25 13:40:59 +0000

    stretagy commit

--- Vue.Net/VIAT.WorkFlow/Services/PriceStretagy/Partial/Viat_wk_contract_stretagyService.cs
+++ Vue.Net/VIAT.WorkFlow/Services/PriceStretagy/Partial/Viat_wk_contract_stretagyService.cs
@@ -17,6 +17,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using VIAT.WorkFlow.IRepositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace VIAT.WorkFlow.Services
 {
@@ -39,7 +41,119 @@ namespace VIAT.WorkFlow.Services
             //base.Init(dbRepository);
         }
 
-      
+        WebResponseContent webResponse = new WebResponseContent();
+
+        /// <summary>
+        /// add
+        /// </summary>
+        /// <param name="saveDataModel"></param>
+        /// <returns></returns>
+        public override WebResponseContent Add(SaveModel saveDataModel)
+        {
+            return processData(saveDataModel);
+        }
+
+        /// <summary>
+        /// update
+        /// </summary>
+        /// <param name="saveModel"></param>
+        /// <returns></returns>
+        public override WebResponseContent Update(SaveModel saveModel)
+        {
+            return processData(saveModel);
+        }
+
+
+        /// <summary>
+        /// 处理数据
+        /// </summary>
+        /// <param name="saveModel"></param>
+        /// <returns></returns>
+        public WebResponseContent processData(SaveModel saveDataModel)
+        {
+            //解析数据
+            //处理表头
+            SaveModel.DetailListDataResult headResult = new SaveModel.DetailListDataResult();
+            string contstret_dbid = "";
+            Viat_wk_contract_stretagy head = new Viat_wk_contract_stretagy();
+            head = JsonConvert.DeserializeObject<Viat_wk_contract_stretagy>(JsonConvert.SerializeObject(saveDataModel.MainData));
+            if(head.contstret_dbid!= null && head.contstret_dbid.ToString() != getDefaultGuid(typeof(Viat_wk_contract_stretagy)))
+            {
+                //更新
+                contstret_dbid = saveDataModel.MainData["contstret_dbid"].ToString();
+                headResult.optionType = SaveModel.MainOptionType.update;
+            }
+            else
+            {
+                contstret_dbid= System.Guid.NewGuid().ToString();
+
+                //新增
+                headResult.optionType = SaveModel.MainOptionType.add;
+                saveDataModel.MainData["contstret_dbid"] = contstret_dbid;
+            }
+            
+            headResult.detailType = typeof(Viat_wk_contract_stretagy);
+            //增加表头处理
+            headResult.DetailData.Add(JsonConvert.DeserializeObject<Dictionary<string,object>>(JsonConvert.SerializeObject(head)));
+            saveDataModel.DetailListData.Add(headResult);
+
+            //处理表体
+            if (saveDataModel.DetailData != null && saveDataModel.DetailData.Count > 0)
+            {
+                //新增
+                SaveModel.DetailListDataResult insertResult = new SaveModel.DetailListDataResult();
+                insertResult.optionType = SaveModel.MainOptionType.add;
+                insertResult.detailType = typeof(Viat_wk_cont_stretagy_detail);
+                saveDataModel.DetailListData.Add(insertResult);
+                SaveModel.DetailListDataResult updateResult = new SaveModel.DetailListDataResult();
+                updateResult.optionType = SaveModel.MainOptionType.update;
+                updateResult.detailType = typeof(Viat_wk_cont_stretagy_detail);
+                saveDataModel.DetailListData.Add(updateResult);
+                foreach (Dictionary<string, object> dic in saveDataModel.DetailData)
+                {
+                    Dictionary<string, object> dicTmp = dic;
+                    if (dicTmp["key"]?.ToString() == "table1RowData")
+                    {
+                        string sData = dicTmp["value"]?.ToString();
+                        //新增
+                        List<Viat_wk_cont_stretagy_detail> list = JsonConvert.DeserializeObject<List<Viat_wk_cont_stretagy_detail>>(sData);
+                        foreach(Viat_wk_cont_stretagy_detail deatil in list)
+                        {
+                            if(deatil.contstretail_dbid!=null && deatil.contstretail_dbid.ToString() != getDefaultGuid(typeof(Viat_wk_cont_stretagy_detail)))
+                            {
+                                //更新
+                                updateResult.DetailData.Add(JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(deatil)));
+                            }
+                            else
+                            {
+                                //新增
+                                deatil.contstretail_dbid = System.Guid.NewGuid();
+                                deatil.contstret_dbid = head.contstret_dbid;
+                                insertResult.DetailData.Add(JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(deatil)));
+
+                            }
+                        }
+                    }                    
+                    else if (dicTmp["key"]?.ToString() == "delTable1RowData")
+                    {
+                        string sDeleteData = dicTmp["value"]?.ToString();
+                        //删除
+                        SaveModel.DetailListDataResult deleteResult = new SaveModel.DetailListDataResult();
+                        deleteResult.optionType = SaveModel.MainOptionType.delete;
+                        deleteResult.detailType = typeof(Viat_wk_cont_stretagy_detail);
+                        saveDataModel.DetailListData.Add(deleteResult);
+                        List<Viat_wk_cont_stretagy_detail> deleteList = JsonConvert.DeserializeObject<List<Viat_wk_cont_stretagy_detail>>(sDeleteData);
+                        foreach(Viat_wk_cont_stretagy_detail detail in deleteList)
+                        {
+                            deleteResult.detailDelKeys.Add(detail.contstretail_dbid);
+                        }
+                    }
+                }
+            }
+            base.CustomBatchProcessEntity(saveDataModel);
+            webResponse.Code = "-1";
+            return webResponse.OK();
+        }
 
     }
 }
