aa5ddab1d4569564c01243af7bde91efa137eb1b
bu <bu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-08-05 09:35:51 +0000

    BPM Approve Setting

---
+++ VIAT.WorkFlow/IServices/setting/Partial/IViat_wk_approve_level_settingService.cs
@@ -0,0 +1,13 @@
+/*
+*所有关于Viat_wk_approve_level_setting类的业务代码接口应在此处编写
+*/
+using VIAT.Core.BaseProvider;
+using VIAT.Entity.DomainModels;
+using VIAT.Core.Utilities;
+using System.Linq.Expressions;
+namespace VIAT.WorkFlow.IServices
+{
+    public partial interface IViat_wk_approve_level_settingService
+    {
+    }
+ }
