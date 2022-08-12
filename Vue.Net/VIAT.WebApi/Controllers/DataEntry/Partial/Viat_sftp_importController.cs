418dbb3a824f02213915fd8852df09dd1abe5735
dragonlin <dragonlin@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-08-12 09:45:27 +0000

    sftp import controller

--- Vue.Net/VIAT.WebApi/Controllers/DataEntry/Partial/Viat_sftp_importController.cs
+++ Vue.Net/VIAT.WebApi/Controllers/DataEntry/Partial/Viat_sftp_importController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using VIAT.Entity.DomainModels;
 using VIAT.DataEntry.IServices;
+using VIAT.Core.Utilities;
 
 namespace VIAT.DataEntry.Controllers
 {
@@ -29,5 +30,89 @@ namespace VIAT.DataEntry.Controllers
             _service = service;
             _httpContextAccessor = httpContextAccessor;
         }
+
+        public override ActionResult GetPageData([FromBody] PageDataOptions loadData)
+        {
+            string distId = "";
+            string source = "";
+            if (!string.IsNullOrEmpty( loadData.Wheres))
+            {
+                List<Dictionary<string, string>> whereList=  Newtonsoft.Json.JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(loadData.Wheres);
+                if(whereList != null && whereList.Count > 0)
+                {
+                    foreach(Dictionary<string, string> w in whereList)
+                    {
+                        if(w["name"] == "dist_id")
+                        {
+                            distId = w["value"];
+                        }
+                        if (w["name"] == "source")
+                        {
+                            source = w["value"];
+                        }
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(distId))
+            {
+                return Json(new WebResponseContent { Status = false, Message = "請至少選取一個經銷商" });
+            }
+
+            List<Viat_sftp_export> result = _service.queryCSVFromSftp(distId, source);
+            return Json(result);
+            //return base.GetPageData(loadData);
+        }
+
+        [HttpPost, Route("doImportCSVFromSftp")]
+        public IActionResult DoImportCSVFromSftp([FromBody] SftpImportViewModel options)
+        {
+            if(string.IsNullOrEmpty(options.DistId))
+            {
+                return Json(new WebResponseContent { Status = false, Message = "請至少選取一個經銷商" });
+            }
+
+            if(options.FileNames == null || options.FileNames.Length ==0)
+            {
+                return Json(new WebResponseContent { Status = false, Message = "請至少選取一個檔案" });
+            }
+
+            _service.doImportCSVFromSftp(options.DistId, options.FileNames);
+
+            return Json(new WebResponseContent { Status = true });
+        }
+
+
+        public override ActionResult Import(List<IFormFile> fileInput)
+        {
+            if (fileInput == null || fileInput.Count == 0)
+            {
+                return Json(new WebResponseContent { Status = false, Message = "請至少選取一個檔案" });
+            }
+            int c = fileInput.Count;
+            _service.doImportCSVFromFile(fileInput);
+            return Json(new WebResponseContent { Status = true });
+        }
+
+      
+
+        [HttpPost, Route("doExecute")]
+        public IActionResult DoExecute([FromBody] Dictionary<string,string> options)
+        {
+            int c = options.Count;
+            List<Viat_sftp_export>  result=_service.queryCSVFromSftp("2", "");
+            //string[] fileNames = { "sales_3_20220708191938.csv" };
+            //string[] fileNames = { "sales_3_20220707191938.csv" };
+            //string[] fileNames = { "invdist_3_2022070819.csv" , "invpfizer_3_2022070819.csv" };
+            
+            //_service.doImportCSVFromFile("D:\\Documents\\MyProject\\耀得\\SFTP Import", fileNames);
+            return Json(options);
+        }
+    }
+
+    public class SftpImportViewModel
+    {
+        public string DistId { get; set; }
+        public string Source { get; set; }
+        public string[] FileNames { get; set; }
     }
 }
