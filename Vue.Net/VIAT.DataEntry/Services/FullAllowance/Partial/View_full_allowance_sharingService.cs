8d4459944415dc4797024cdc296e73214b65b55f
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-08-12 11:15:47 +0000

    重写查询

--- Vue.Net/VIAT.DataEntry/Services/FullAllowance/Partial/View_full_allowance_sharingService.cs
+++ Vue.Net/VIAT.DataEntry/Services/FullAllowance/Partial/View_full_allowance_sharingService.cs
@@ -17,6 +17,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using VIAT.DataEntry.IRepositories;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace VIAT.DataEntry.Services
 {
@@ -37,5 +39,56 @@ namespace VIAT.DataEntry.Services
             //多租户会用到这init代码，其他情况可以不用
             //base.Init(dbRepository);
         }
-  }
+
+        public override PageGridData<View_full_allowance_sharing> GetPageData(PageDataOptions options)
+        {
+            PageGridData<View_full_allowance_sharing> pageGridData = new PageGridData<View_full_allowance_sharing>();
+            /*解析查询条件*/
+            List<SearchParameters> searchParametersList = new List<SearchParameters>();
+            string hpcont_dbid = "";
+            string hpcont_dbid1 = "";
+            if (!string.IsNullOrEmpty(options.Wheres))
+            {
+                searchParametersList = options.Wheres.DeserializeObject<List<SearchParameters>>();
+                if (searchParametersList != null && searchParametersList.Count > 0)
+                {
+                    foreach (SearchParameters sp in searchParametersList)
+                    {
+                        if (sp.Name.ToLower() == "hpcont_dbid".ToLower())
+                        {
+                            hpcont_dbid = " and allw_sum.hpcont_dbid='" + sp.Value + "'";
+                            hpcont_dbid1 = " and share.hpcont_dbid='" + sp.Value + "'";
+                            continue;
+                        }
+                    }
+                }
+
+            }
+
+            QuerySql = "select ROW_NUMBER()over(order by tab4.cust_id desc) as rowId, tab4.* from( " +
+                "select tab.*,cust.cust_id,cust.cust_name,cust.status,prod.prod_id,prod.prod_ename,prod.state,tab2.amount,tab3.[percent] as [percent] from( "+
+                "select allw_sum.prod_dbid as prod_dbid, allw_sum.cust_dbid as cust_dbid, sum(allw_sum.amount) as A1 from viat_app_hp_contract_allw_sum allw_sum "+
+                 "left outer join Sys_User s_user  on s_user.User_Id = allw_sum.modified_user " +
+                "left outer join viat_com_employee employee on employee.emp_dbid = s_user.emp_dbid " +
+                "where allw_sum.prod_dbid is not null and allw_sum.cust_dbid is not null and allw_sum.action_type = '1' " +
+                hpcont_dbid+
+                " group by allw_sum.prod_dbid, allw_sum.cust_dbid " +
+                ")tab " +
+                "LEFT OUTER JOIN  viat_com_cust cust on cust.cust_dbid = tab.cust_dbid " +
+                "left outer join viat_com_prod prod on prod.prod_dbid = tab.prod_dbid " +
+                "outer apply(select top 1 allw_sum.amount as amount from viat_app_hp_contract_allw_sum allw_sum where tab.cust_dbid = allw_sum.cust_dbid and tab.prod_dbid = allw_sum.prod_dbid) as tab2 " +
+                "outer apply(select top 1 share.[percent] as [percent]  from viat_app_hp_contract_share share where share.cust_dbid = tab.cust_dbid and share.prod_dbid = tab.prod_dbid " +
+                 hpcont_dbid1 +
+                " ) as tab3 " +
+               " )tab4";
+            string sql = "select count(1) from (" + QuerySql + ") a";
+            pageGridData.total = repository.DapperContext.ExecuteScalar(sql, null).GetInt();
+
+            sql = @$"select * from (" +
+                QuerySql + $" ) as s where s.rowId between {((options.Page - 1) * options.Rows + 1)} and {options.Page * options.Rows} ";
+            pageGridData.rows = repository.DapperContext.QueryList<View_full_allowance_sharing>(sql, null);
+
+            return pageGridData;
+        }
+    }
 }
